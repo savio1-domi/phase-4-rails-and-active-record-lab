dbe761b65f7e36e4033cb03a7ac19402e7e9ebc9
git-savio1.domi <techsavvydominic3@gmail.com>
2023-06-19 23:35:15 +0300

    Completed assignments

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Student, type: :model do
+RSpec.describe 'Student', type: :model do
   let!(:student) { Student.new(first_name: 'Dwayne', last_name: 'Johnson', grade: 99) }
 
   describe '#first_name' do
